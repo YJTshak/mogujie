f8b4b1f8ba2f98ad1f5b2dd284d0a8aa717fa4b7
shaking <1091605434@qq.com>
2018-08-22 22:40:16 +0800

    轮播图

--- src/js/nav.js
+++ src/js/nav.js
@@ -123,5 +123,9 @@ document.addEventListener('DOMContentLoaded',()=>{
             $('.banner ul').gsCarousel({
                 imgs:['../img/l1.jpg','../img/l2.jpg','../img/l3.jpg'],
             }).show();
+
+            
         })
+
+        
         
